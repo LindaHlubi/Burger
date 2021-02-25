c61002611d71c4390e1aaa1e8e17dce5c0c7223d
LINDA_HLUBI <linda_hlubi@icloud.com>
2021-02-25 17:42:36 +1100

    commit updateOne orm

--- config/orm.js
+++ config/orm.js
@@ -34,6 +34,25 @@ insertOne: function(table, burger_name, cb) {
     });
 },
 
+updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+  
 }
+
+
 
 
