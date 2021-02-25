f8ab7a189abe676af33ba892ea1d8612b6d22620
LINDA_HLUBI <linda_hlubi@icloud.com>
2021-02-25 16:50:22 +1100

    commit schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INTEGER NOT NULL AUTO_INCREMENT, 
+    PRIMARY KEY (id),
+    burger_name VARCHAR(500)
+    devoured BOOLEAN DEFAULT false
+);
+
